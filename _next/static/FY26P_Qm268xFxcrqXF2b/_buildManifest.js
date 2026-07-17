f00637dacedb58cc79ca216c3215b7502685b3e0
Josh Zhao <zhaokvance@gmail.com>
2026-07-17 09:40:10 +0800

    Deploy static site to GitHub Pages

---
+++ _next/static/FY26P_Qm268xFxcrqXF2b/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/arena-final-20260717//_next/:path+",
+        "destination": "/arena-final-20260717/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
